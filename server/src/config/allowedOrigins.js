1556725980b485ba1968eb68db6ddd49e2799d8d
Zen-cronic <83657429+Zen-cronic@users.noreply.github.com>
2023-11-13 11:08:03 -0500

    added ending / in allowedOrigin

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -3,8 +3,7 @@ const allowedOrigins = [
     // 'http://localhost:5173',
     // 'http://localhost:5174',
 
-    'https://volun-app-monorepo-production-sample-fyvdo2cjo-zen-cronic.vercel.app/',
-    'https://volun-app-monorepo-production-sample.vercel.app',
+    'https://volun-app-monorepo-production-sample.vercel.app/',
    
 
 ]
